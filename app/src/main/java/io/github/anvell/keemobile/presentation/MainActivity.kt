39df5de73681a5987363eb39bd50941b57975d46
Denis Trotsenko <denis.tnko@gmail.com>
2019-07-23 23:27:09 +0200

    Created Android project

---
+++ app/src/main/java/io/github/anvell/keemobile/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.anvell.keemobile.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.anvell.keemobile.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
