03a2e385011ceb22bd7628574813ba18fbafc9c2
Denis Trotsenko <denis.tnko@gmail.com>
2019-07-28 15:41:43 +0200

    Setting up dependencies

--- app/src/main/java/io/github/anvell/keemobile/KeeMobileApp.kt
+++ app/src/main/java/io/github/anvell/keemobile/KeeMobileApp.kt
@@ -2,11 +2,16 @@ package io.github.anvell.keemobile
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
 
 class KeeMobileApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
