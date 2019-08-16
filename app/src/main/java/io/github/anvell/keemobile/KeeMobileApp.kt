6353521b6ff7b4e85b336b08bca41190c83dbf55
Denis Trotsenko <denis.tnko@gmail.com>
2019-08-16 23:45:29 +0200

    Apply night theme by default

--- app/src/main/java/io/github/anvell/keemobile/KeeMobileApp.kt
+++ app/src/main/java/io/github/anvell/keemobile/KeeMobileApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
 import io.github.anvell.keemobile.di.DaggerAppComponent
 import io.github.anvell.keemobile.di.InjectorProvider
+import io.github.anvell.keemobile.presentation.ui.DayNightSwitcher
 import timber.log.Timber
 
 class KeeMobileApp : Application(), InjectorProvider {
@@ -19,5 +20,6 @@ class KeeMobileApp : Application(), InjectorProvider {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        DayNightSwitcher.apply(DayNightSwitcher.NIGHT)
     }
 }
