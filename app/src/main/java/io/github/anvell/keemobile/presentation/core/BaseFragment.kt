c9036450fbf10f825fdb4bc65e3793b51980b2a2
Denis Trotsenko <denis.tnko@gmail.com>
2019-08-17 00:31:52 +0200

    Updated base fragment

--- app/src/main/java/io/github/anvell/keemobile/presentation/core/BaseFragment.kt
+++ app/src/main/java/io/github/anvell/keemobile/presentation/core/BaseFragment.kt
@@ -1,5 +1,52 @@
 package io.github.anvell.keemobile.presentation.core
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import com.airbnb.mvrx.BaseMvRxFragment
+import io.github.anvell.keemobile.common.constants.RequestCodes
 
-abstract class BaseFragment : BaseMvRxFragment()
+abstract class BaseFragment<T>(
+    val inflaterBlock: (
+        inflater: LayoutInflater,
+        root: ViewGroup?, attachToRoot: Boolean
+    ) -> T
+) : BaseMvRxFragment() where T : ViewDataBinding {
+
+    protected lateinit var binding: T
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = inflaterBlock(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            RequestCodes.FILE_OPEN -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.data?.also {
+                        onFileOpened(it)
+                    }
+                }
+            }
+        }
+    }
+
+    protected fun requestOpenFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            flags = flags and (Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            type = "*/*"
+        }
+        startActivityForResult(intent, RequestCodes.FILE_OPEN)
+    }
+
+    protected open fun onFileOpened(uri: Uri) = Unit
+}
