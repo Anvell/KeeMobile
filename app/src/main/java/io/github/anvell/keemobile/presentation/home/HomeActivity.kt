f9c9448ab74485537d44901474db88e4f46b9cb3
Denis Trotsenko <denis.tnko@gmail.com>
2019-08-17 00:30:36 +0200

    Rename home activity

--- app/src/main/java/io/github/anvell/keemobile/presentation/home/HomeActivity.kt
+++ app/src/main/java/io/github/anvell/keemobile/presentation/home/HomeActivity.kt
@@ -1,9 +1,8 @@
 package io.github.anvell.keemobile.presentation.home
 
 import android.os.Bundle
-import com.airbnb.mvrx.BaseMvRxActivity
 import io.github.anvell.keemobile.R
-import io.github.anvell.keemobile.extensions.injector
+import io.github.anvell.keemobile.common.extensions.injector
 import io.github.anvell.keemobile.presentation.core.BaseActivity
 import javax.inject.Inject
 
@@ -15,6 +14,6 @@ class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         injector.inject(this)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
