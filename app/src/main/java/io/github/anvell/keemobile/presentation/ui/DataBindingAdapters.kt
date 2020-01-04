04a5392d1c4df19837bbe1b326ec6f8899967f1f
Denis Trotsenko <denis.tnko@gmail.com>
2020-01-05 00:48:31 +0100

    Added widget for tag cloud

--- app/src/main/java/io/github/anvell/keemobile/presentation/ui/DataBindingAdapters.kt
+++ app/src/main/java/io/github/anvell/keemobile/presentation/ui/DataBindingAdapters.kt
@@ -1,12 +1,12 @@
 package io.github.anvell.keemobile.presentation.ui
 
 import android.content.res.ColorStateList
+import android.view.View
 import android.widget.ImageView
 import androidx.annotation.ColorInt
-import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
 import androidx.databinding.BindingAdapter
-import io.github.anvell.keemobile.R
+import io.github.anvell.keemobile.presentation.widgets.TagsCloudView
 
 object DataBindingAdapters {
 
@@ -22,5 +22,19 @@ object DataBindingAdapters {
         color?.let {
             ImageViewCompat.setImageTintList(imageView, ColorStateList.valueOf(it))
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter("onLongClick")
+    fun setOnLongClick(view: View, listener: View.OnLongClickListener?) {
+        listener?.let {
+            view.setOnLongClickListener(listener)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("tags")
+    fun setTags(view: TagsCloudView, tags: List<String>) {
+        view.setTags(tags)
     }
 }
