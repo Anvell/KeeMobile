53fffc5335832fbda891666fada67159a83793d8
Denis Trotsenko <denis.tnko@gmail.com>
2020-01-06 23:54:37 +0100

    Prepare layouts for details fragment

--- app/src/main/java/io/github/anvell/keemobile/presentation/ui/package-info.java
+++ app/src/main/java/io/github/anvell/keemobile/presentation/ui/package-info.java
@@ -3,7 +3,11 @@
         R.layout.item_drawer,
         R.layout.item_entry,
         R.layout.item_info,
-        R.layout.item_header
+        R.layout.item_header,
+        R.layout.item_details_property,
+        R.layout.item_details_property_masked,
+        R.layout.item_details_header,
+        R.layout.item_details_tags
 })
 package io.github.anvell.keemobile.presentation.ui;
 
