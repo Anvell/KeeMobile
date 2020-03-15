baa51444c64a53038fe1d906a26cbef2c532762e
Denis Trotsenko <denis.tnko@gmail.com>
2020-03-15 17:49:26 +0100

    Added spring animation helper for views

--- app/src/main/java/io/github/anvell/keemobile/common/extensions/View.kt
+++ app/src/main/java/io/github/anvell/keemobile/common/extensions/View.kt
@@ -3,6 +3,10 @@ package io.github.anvell.keemobile.common.extensions
 import android.graphics.Outline
 import android.view.View
 import android.view.ViewOutlineProvider
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
+import io.github.anvell.keemobile.common.mapper.ViewPropertyMapper
 
 fun View.clipToCornerRadius(radius: Float) {
     outlineProvider = object : ViewOutlineProvider() {
@@ -11,4 +15,29 @@ fun View.clipToCornerRadius(radius: Float) {
         }
     }
     clipToOutline = true
+}
+
+fun View.springAnimation(
+    property: DynamicAnimation.ViewProperty,
+    stiffness: Float = SpringForce.STIFFNESS_LOW,
+    dampingRatio: Float = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY,
+    velocity: Float = 0f
+): SpringAnimation {
+    val key = ViewPropertyMapper.toId(property)
+    var animation = getTag(key) as? SpringAnimation?
+
+    if (animation == null) {
+        animation = SpringAnimation(this, property)
+        setTag(key, animation)
+    }
+
+    with(animation) {
+        spring = (animation.spring ?: SpringForce()).apply {
+            this.stiffness = stiffness
+            this.dampingRatio = dampingRatio
+        }
+        setStartVelocity(velocity)
+    }
+
+    return animation
 }
