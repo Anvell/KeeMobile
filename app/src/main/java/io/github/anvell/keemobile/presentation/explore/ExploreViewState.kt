ffdaa7dc80e0b7d880997b0b186f9914095db329
Denis Trotsenko <denis.trotsenko@ergo.digital>
2020-03-21 16:23:11 +0100

    Fixed navigation state persistence on explore screen

--- app/src/main/java/io/github/anvell/keemobile/presentation/explore/ExploreViewState.kt
+++ app/src/main/java/io/github/anvell/keemobile/presentation/explore/ExploreViewState.kt
@@ -2,6 +2,7 @@ package io.github.anvell.keemobile.presentation.explore
 
 import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.MvRxState
+import com.airbnb.mvrx.PersistState
 import com.airbnb.mvrx.Uninitialized
 import io.github.anvell.keemobile.domain.alias.VaultId
 import io.github.anvell.keemobile.domain.entity.AppSettings
@@ -11,7 +12,7 @@ import java.util.*
 
 data class ExploreViewState(
     val activeDatabaseId: VaultId,
-    val rootStack: List<UUID> = listOf(),
+    @PersistState val rootStack: List<UUID> = listOf(),
     val activeDatabase: Async<OpenDatabase> = Uninitialized,
     val searchResults: Async<SearchResults> = Uninitialized,
     val appSettings: Async<AppSettings> = Uninitialized
--- app/src/main/java/io/github/anvell/keemobile/presentation/widgets/AnimatedImageView.kt
+++ app/src/main/java/io/github/anvell/keemobile/presentation/widgets/AnimatedImageView.kt
@@ -3,6 +3,8 @@ package io.github.anvell.keemobile.presentation.widgets
 import android.content.Context
 import android.graphics.drawable.Animatable
 import android.graphics.drawable.Drawable
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import io.github.anvell.keemobile.R
@@ -11,9 +13,10 @@ class AnimatedImageView(context: Context, attrs: AttributeSet?) : AppCompatImage
 
     private val animDrawable: Drawable?
     private val animReversedDrawable: Drawable?
+    private var isAtStart = true
 
     init {
-        animDrawable = getDrawable()
+        animDrawable = drawable
         val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.AnimatedImageView, 0, 0)
 
         try {
@@ -24,10 +27,12 @@ class AnimatedImageView(context: Context, attrs: AttributeSet?) : AppCompatImage
     }
 
     fun rewindToStart() {
+        isAtStart = true
         setImageDrawable(animDrawable)
     }
 
     fun rewindToEnd() {
+        isAtStart = false
         setImageDrawable(animReversedDrawable)
     }
 
@@ -37,6 +42,7 @@ class AnimatedImageView(context: Context, attrs: AttributeSet?) : AppCompatImage
         if (animDrawable != null && animDrawable is Animatable) {
             with(animDrawable as Animatable) {
                 if (!isRunning) {
+                    isAtStart = false
                     start()
                 }
             }
@@ -49,7 +55,55 @@ class AnimatedImageView(context: Context, attrs: AttributeSet?) : AppCompatImage
         if (animReversedDrawable != null && animReversedDrawable is Animatable) {
             with(animReversedDrawable as Animatable) {
                 if (!isRunning) {
+                    isAtStart = true
                     start()
+                }
+            }
+        }
+    }
+
+    private fun setPosition(isAtStart: Boolean) = when (isAtStart) {
+        true -> rewindToStart()
+        else -> rewindToEnd()
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        return SavedState(super.onSaveInstanceState()).apply {
+            isAtStart = this@AnimatedImageView.isAtStart
+        }
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+        isAtStart = savedState.isAtStart
+        setPosition(isAtStart)
+    }
+
+    internal class SavedState : BaseSavedState {
+        var isAtStart = true
+
+        constructor(superState: Parcelable?) : super(superState)
+
+        constructor(parcel: Parcel) : super(parcel) {
+            isAtStart = parcel.readBoolean()
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeBoolean(isAtStart)
+        }
+
+        companion object {
+
+            @JvmField
+            val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(parcel: Parcel): SavedState {
+                    return SavedState(parcel)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls(size)
                 }
             }
         }
