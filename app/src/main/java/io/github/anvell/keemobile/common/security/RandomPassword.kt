d59dfb943e6d93200b02b1d82d092396c3ef164b
Denis Trotsenko <denis.tnko@gmail.com>
2020-04-18 01:24:55 +0200

    Added simple password generator

---
+++ app/src/main/java/io/github/anvell/keemobile/common/security/RandomPassword.kt
@@ -0,0 +1,28 @@
+package io.github.anvell.keemobile.common.security
+
+import java.security.SecureRandom
+
+object RandomPassword {
+    private val generator = SecureRandom()
+
+    fun create(length: Int, vararg dictionaries: String = Dictionaries.all()): String {
+        return (0 until length).fold("") { acc, _ ->
+            val subset = dictionaries[generator.nextInt(dictionaries.size)]
+            acc + subset[generator.nextInt(subset.length)]
+        }
+    }
+
+    object Dictionaries {
+        const val LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+        const val UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        const val DIGITS = "0123456789"
+        const val SYMBOLS = """!"#$%&'()*+,-./:;<=>?@[]^_{|}~"""
+
+        fun all() = arrayOf(
+            LOWERCASE_LETTERS,
+            UPPERCASE_LETTERS,
+            DIGITS,
+            SYMBOLS
+        )
+    }
+}
