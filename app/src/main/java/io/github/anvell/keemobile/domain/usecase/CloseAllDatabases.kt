2a7a22fdf4c6ec5c39bbe76609bd4aaa8660f16d
Denis Trotsenko <denis.tnko@gmail.com>
2020-05-02 18:23:31 +0200

    Close all files when navigating up from the root of explore screen

--- app/src/main/java/io/github/anvell/keemobile/domain/usecase/CloseAllDatabases.kt
+++ app/src/main/java/io/github/anvell/keemobile/domain/usecase/CloseAllDatabases.kt
@@ -1,15 +1,14 @@
 package io.github.anvell.keemobile.domain.usecase
 
 import dagger.Reusable
-import io.github.anvell.keemobile.domain.alias.VaultId
 import io.github.anvell.keemobile.domain.repository.DatabaseRepository
-import io.reactivex.Single
+import io.reactivex.Completable
 import javax.inject.Inject
 
 @Reusable
-class CloseDatabase @Inject constructor(
+class CloseAllDatabases @Inject constructor(
     private val databaseRepository: DatabaseRepository
 ) {
 
-    fun use(id: VaultId) = Single.fromCallable { databaseRepository.close(id) }
+    fun use() = Completable.fromAction { databaseRepository.closeAll() }
 }
