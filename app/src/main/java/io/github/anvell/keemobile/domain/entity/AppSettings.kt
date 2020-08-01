626e974934647ab396a887ea0229ad22653a886c
Denis Trotsenko <denis.tnko@gmail.com>
2020-08-01 18:51:27 +0200

    Move ViewMode

--- app/src/main/java/io/github/anvell/keemobile/domain/entity/AppSettings.kt
+++ app/src/main/java/io/github/anvell/keemobile/domain/entity/AppSettings.kt
@@ -2,6 +2,11 @@ package io.github.anvell.keemobile.domain.entity
 
 import com.squareup.moshi.JsonClass
 
+enum class ViewMode {
+    TREE,
+    LIST
+}
+
 @JsonClass(generateAdapter = true)
 data class AppSettings(
     val exploreViewMode: ViewMode = ViewMode.TREE
--- app/src/main/java/io/github/anvell/keemobile/domain/entity/ViewMode.kt
+++
@@ -1,6 +0,0 @@
-package io.github.anvell.keemobile.domain.entity
-
-enum class ViewMode {
-    TREE,
-    LIST
-}
