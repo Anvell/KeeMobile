f86093f9035a781d16415572b49e9eb2b09d0d48
Denis Trotsenko <denis.tnko@gmail.com>
2021-02-21 20:44:58 +0100

    Added compose fragment and viewmodel classes

---
+++ core-ui/src/main/java/io/github/anvell/keemobile/core/ui/mvi/MviComposeFragment.kt
@@ -0,0 +1,53 @@
+package io.github.anvell.keemobile.core.ui.mvi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+abstract class MviComposeFragment<V, S, C> : Fragment() where V : MviComposeViewModel<S, C> {
+    private val pendingCommands = MutableSharedFlow<C>()
+
+    abstract val viewModel: V
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            pendingCommands.collect { onCommand(it) }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = ComposeView(requireContext()).apply {
+        setContent {
+            viewModel.observableState()
+                .observeAsState()
+                .value
+                ?.let { state ->
+                    Сontent(
+                        state = state,
+                        commands = {
+                            lifecycleScope.launch { pendingCommands.emit(it) }
+                        }
+                    )
+                }
+        }
+    }
+
+    protected open fun onCommand(command: C) = viewModel.emitCommand(command)
+
+    @Composable
+    abstract fun Сontent(state: S, commands: (C) -> Unit)
+}
---
+++ core-ui/src/main/java/io/github/anvell/keemobile/core/ui/mvi/MviViewModel.kt
@@ -0,0 +1,99 @@
+package io.github.anvell.keemobile.core.ui.mvi
+
+import androidx.lifecycle.*
+import io.github.anvell.keemobile.domain.datatypes.*
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlin.reflect.KProperty1
+
+abstract class MviViewModel<S>(initialState: S) : ViewModel() {
+    private val state = MutableStateFlow(initialState)
+
+    fun observableState() = state.asLiveData()
+
+    fun <T> withState(block: (S) -> T) = block(state.value)
+
+    protected fun setState(reducer: S.() -> S) {
+        state.value = reducer(state.value)
+    }
+
+    protected fun stateSubscribe(block: (S) -> Unit) {
+        viewModelScope.launch {
+            state.collect { block(it) }
+        }
+    }
+
+    fun <P> selectSubscribe(property: KProperty1<S, P>): LiveData<P> {
+        return selectSubscribeInternal(property).asLiveData()
+    }
+
+    protected fun <P> selectSubscribe(property: KProperty1<S, P>, block: (P) -> Unit) {
+        viewModelScope.launch {
+            selectSubscribeInternal(property).collect { block(it) }
+        }
+    }
+
+    private fun <P> selectSubscribeInternal(property: KProperty1<S, P>): Flow<P> {
+        return state.map { property.get(it) }.distinctUntilChanged()
+    }
+
+    protected suspend fun <V> Flow<Either<Throwable, V>>.execute(reducer: S.(Async<V>) -> S) {
+        catch { setState { reducer(Fail(it)) } }
+        collect {
+            it.fold(
+                left = { setState { reducer(Fail(it)) } },
+                right = { setState { reducer(Success(it)) } }
+            )
+        }
+    }
+
+    protected suspend fun <V> Flow<V>.executeCatching(reducer: S.(Async<V>) -> S) {
+        catch { setState { reducer(Fail(it)) } }
+        collect { setState { reducer(Success(it)) } }
+    }
+
+    protected fun <V> execute(
+        block: suspend S.() -> Either<Throwable, V>,
+        reducer: S.(Async<V>) -> S
+    ) {
+        viewModelScope.launch {
+            setState { reducer(Loading) }
+
+            block(state.value).fold(
+                left = { setState { reducer(Fail(it)) } },
+                right = { setState { reducer(Success(it)) } }
+            )
+        }
+    }
+
+    protected fun <V> executeCatching(
+        block: suspend S.() -> V,
+        reducer: S.(Async<V>) -> S
+    ) {
+        viewModelScope.launch {
+            setState { reducer(Loading) }
+
+            try {
+                val result = block(state.value)
+                setState { reducer(Success(result)) }
+            } catch (error: Throwable) {
+                setState { reducer(Fail(error)) }
+            }
+        }
+    }
+
+    protected inline fun <reified P> SavedStateHandle.rememberAsJson(property: KProperty1<S, P>) {
+        selectSubscribe(property) { this[property.name] = Json.encodeToString(it) }
+    }
+
+    companion object {
+        inline fun <S, reified P> SavedStateHandle.retrieveFromJson(property: KProperty1<S, P>): P? {
+            return get<String>(property.name)?.let {
+                Json { coerceInputValues = true }.decodeFromString(it)
+            }
+        }
+    }
+}
